76c088365c04da998e42d375c3f0ddc5dcf6e1fe
confused-Techie <dev@lhbasics.com>
2023-04-12 18:19:12 -0700

    Initial setup

---
+++ site.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  config: {
+    sourceDirectory: "./docs",
+    buildDirectory: "./dist",
+    devPort: 8080,
+    jsBuildDirectory: "./dist/js",
+    jsSourceDirectory: "./assets/js",
+    jsMinifyGenerateSourceMap: true,
+    jsMinifyOptions: {},
+    cssBuildDirectory: "./dist/css",
+    cssSourceDirectory: "./assets/css",
+    cssMinifyGenerateSourceMap: true,
+    cssMinifyOptions: {},
+    staticSourceDirectory: "./assets/static",
+    staticBuildDirectory: "./dist",
+    defaultView: "doc",
+    viewPagePath: "./views/pages"
+  }
+}
